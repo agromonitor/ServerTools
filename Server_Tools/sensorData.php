99500789dd4a75184c2d1529e65736a05d2ad777
DanielJimenezHernandez <danieljimher@hotmail.es>
2015-11-21 12:16:56 -0800

    Added x86 test and php for each and for sensor data

---
+++ Server_Tools/sensorData.php
@@ -0,0 +1,46 @@
+ <?php
+$servername = "***";
+$usernameDB = "***";
+$passwordDB = "***";
+$dbname = "***";
+$SensorData = "***";
+
+//DataFromQuery from device
+$user = $_POST['user'];
+$password = $_POST['password'];
+$HUM = $_POST['HUM'];
+$TEMP = $_POST['TEMP'];
+$LIGHT = $_POST['LIGHT'];
+$SOIL_HUM = $_POST['SOIL_HUM'];
+
+if (empty($_POST)) {
+	exit("DATA_ERROR");
+}
+else {
+	if(!empty($user) && !empty($password) && !empty($HUM) && !empty($TEMP) && !empty($LIGHT) && !empty($SOIL_HUM)){
+		// Create connection
+		$conn = new mysqli($servername, $usernameDB , $passwordDB, $dbname);
+
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		}
+		//Connected to the database and sending the collected data:
+		$sql = "INSERT INTO ".$SensorData." (HUM, TEMP, LIGHT, SOIL_HUM) VALUES ('".$HUM."', '".$TEMP."', '".$LIGHT."', '".$SOIL_HUM."')";
+
+		if ($conn->query($sql) === TRUE) {
+		    echo "DATA_RECEIVED_OK";
+		} else {
+		    echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+
+		$conn->close();
+
+	}
+
+	else{
+		echo "DATA_INCOMPLETE";
+	}
+
+}
+?>
